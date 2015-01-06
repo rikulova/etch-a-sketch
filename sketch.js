80bb9df7f2d073c00c2905a3111af07cc3d55a3e
Kaela <rikulova@gmail.com>
2015-01-05 22:36:24 -0700

    created populate and button

--- sketch.js
+++ sketch.js
@@ -1,5 +1,19 @@
-$(document).ready(function(){
-	for (var i = 1; i < 17; i++){
+function populate(number){
+	for (var i = 1; i < size; i++){
 		$("#outside").append("<div></div>");
-}
+	};
+};
+var size = 17
+$(document).ready(function(){
+	populate(size);
+		 
+	$("div").mouseenter(function(){
+		$(this).css("background-color", "black");
+	});
+	$("button").click(function(){
+		size  = prompt("How many squares?");
+		$("div").css("background-color", "white");
+		populate(size + 1);
+
+	});
 });
